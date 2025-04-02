e7e8b853a10bedde489c3da44827284706311cdb
Rahul Kumar <rahul.rkumar88628@gmail.com>
2025-04-02 14:21:04 +0530

    added all origion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,11 +108,8 @@ import { io } from "socket.io-client";
 import { useAuth } from "@/context/TokenProvider";
 import { API_BASE_URL, API_Socket_URL } from "@/config/api";
 
-const socket = io(`${API_BASE_URL}`, {
-  transports: ["websocket"],
-  withCredentials: true,
-});
-// // const socket = io("http://localhost:3000", {
+ const socket = io(`${API_Socket_URL}`); // Connect to the server
+// const socket = io("http://localhost:3000", {
 //   transports: ["websocket"],
 //   withCredentials: true,
 // });
