fa532796f90b21ff22eec81773aaadf827070120
Rahul Kumar <rahul.rkumar88628@gmail.com>
2025-04-02 14:05:18 +0530

    added all origion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,12 @@ import { io } from "socket.io-client";
 import { useAuth } from "@/context/TokenProvider";
 import { API_BASE_URL, API_Socket_URL } from "@/config/api";
 
-const socket = io("http://localhost:3000"); // Connect to the server
+// const socket = io("http://localhost:3000"); // Connect to the server
+const socket = io("http://localhost:3000", {
+  transports: ["websocket"],
+  withCredentials: true,
+});
+
 
 interface Message {
   text: string;
